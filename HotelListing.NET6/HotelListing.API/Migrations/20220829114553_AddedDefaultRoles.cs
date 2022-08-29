18b2f054c9bef239e62518f7caffd226246daf72
AIKursat <aynurkursat1999@hotmail.com>
2022-08-29 14:47:45 +0300

    Add Default User Roles

---
+++ HotelListing.NET6/HotelListing.API/Migrations/20220829114553_AddedDefaultRoles.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class AddedDefaultRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "6b5306f0-d242-4c77-94e9-9199593edfbb", "4860790f-1ac5-40a3-85b3-639cd2c7d1f9", "Administrator", "ADMINISTRATOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "75e53cb9-eefc-41bd-9f28-3d9cf0cabaff", "cb77b64f-f1f9-43d8-8355-3e74b210feb1", "User", "USER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6b5306f0-d242-4c77-94e9-9199593edfbb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "75e53cb9-eefc-41bd-9f28-3d9cf0cabaff");
+        }
+    }
+}
