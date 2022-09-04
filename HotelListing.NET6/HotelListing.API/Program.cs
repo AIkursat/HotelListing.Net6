aff359cf4f1a3ac1dd36c8f1f3cd0b4157daf447
AIKursat <aynurkursat1999@hotmail.com>
2022-09-04 15:42:56 +0300

    Add caching

--- HotelListing.NET6/HotelListing.API/Program.cs
+++ HotelListing.NET6/HotelListing.API/Program.cs
@@ -88,6 +88,12 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddResponseCaching(options =>
+{
+    options.MaximumBodySize = 1024;
+    options.UseCaseSensitivePaths = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -102,6 +108,22 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
+
+app.UseResponseCaching();
+
+app.Use(async (context, next) =>
+{
+    context.Response.GetTypedHeaders().CacheControl =
+    new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+    {
+        Public = true,
+        MaxAge = TimeSpan.FromSeconds(10),
+    };
+    context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+    new string[] { "Accept-Encoding" };
+
+    await next();
+});
 
 app.UseAuthentication();
 
