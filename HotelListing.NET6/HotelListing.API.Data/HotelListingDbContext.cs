64b4f9511e4fac9b12708eab881843a67e2358ff
AIKursat <aynurkursat1999@hotmail.com>
2022-09-07 00:49:07 +0300

    Add ContextFactory

--- HotelListing.NET6/HotelListing.API.Data/HotelListingDbContext.cs
+++ HotelListing.NET6/HotelListing.API.Data/HotelListingDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using HotelListing.API.Data.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace HotelListing.API.Data
 {
@@ -20,6 +23,24 @@ namespace HotelListing.API.Data
             modelBuilder.ApplyConfiguration(new RoleConfiguration());
             modelBuilder.ApplyConfiguration(new CountryConfiguration());
             modelBuilder.ApplyConfiguration(new HotelConfiguration());
+
+        }
+    }
+
+    public class HotelListingDbContextFactory : IDesignTimeDbContextFactory<HotelListingDbContext>
+    {
+        public HotelListingDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional : false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HotelListingDbContext>();
+            var conn = config.GetConnectionString("HotelListingDbConnectionString");
+            optionsBuilder.UseSqlServer(conn);
+            return new HotelListingDbContext(optionsBuilder.Options);
+
 
         }
     }
