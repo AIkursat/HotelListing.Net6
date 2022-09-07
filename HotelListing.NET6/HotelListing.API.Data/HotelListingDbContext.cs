4bf2efee309db98d6ec854568a292921bd012710
AIKursat <aynurkursat1999@hotmail.com>
2022-09-07 18:18:59 +0300

    Jwt authentication to swagger doc

--- HotelListing.NET6/HotelListing.API.Data/HotelListingDbContext.cs
+++ HotelListing.NET6/HotelListing.API.Data/HotelListingDbContext.cs
@@ -26,22 +26,4 @@ namespace HotelListing.API.Data
 
         }
     }
-
-    public class HotelListingDbContextFactory : IDesignTimeDbContextFactory<HotelListingDbContext>
-    {
-        public HotelListingDbContext CreateDbContext(string[] args)
-        {
-            IConfiguration config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional : false, reloadOnChange: true)
-                .Build();
-
-            var optionsBuilder = new DbContextOptionsBuilder<HotelListingDbContext>();
-            var conn = config.GetConnectionString("HotelListingDbConnectionString");
-            optionsBuilder.UseSqlServer(conn);
-            return new HotelListingDbContext(optionsBuilder.Options);
-
-
-        }
-    }
 }
--- HotelListing.NET6/HotelListing.API/Program.cs
+++ HotelListing.NET6/HotelListing.API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using System.Text;
 
@@ -31,7 +32,39 @@ builder.Services.AddIdentityCore<ApiUser>()
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel Listing API", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+        Enter 'Bearer' [space] and then your token in the text input below.
+        Example: 'Bearer 123455",
+        Name = "Authorization ",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                },
+                Scheme = "0auth2",
+                Name = JwtBearerDefaults.AuthenticationScheme,
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    }); ;
+
+});
 
 builder.Services.AddApiVersioning(options =>
 {
